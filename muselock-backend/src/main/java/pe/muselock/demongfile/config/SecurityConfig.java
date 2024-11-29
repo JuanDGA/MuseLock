37c788f482b7fae0fe6a775e191d9bea723e0eed
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-11-28 22:51:47 -0500

    Completed security config and fixed similarity url

--- muselock-backend/src/main/java/pe/muselock/demongfile/config/SecurityConfig.java
+++ muselock-backend/src/main/java/pe/muselock/demongfile/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package pe.muselock.demongfile.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .authorizeHttpRequests(exchange -> exchange.anyRequest().permitAll())
-        .build();
+    http
+        .securityMatcher("/api/**")
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll()
+        );
+    return http.build();
   }
 }
