5909c80499735cb1570f0f9fdbec3ac67dda5722
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-11-29 00:22:23 -0500

    Updated backend uri

--- muselock-front/src/environments/environment.development.ts
+++ muselock-front/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
-const baseUrl = 'http://localhost:8080/api';
+const baseUrl = 'https://server.muselock.site/api';
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://server.muselock.site/api',
     baseUrl
 };
